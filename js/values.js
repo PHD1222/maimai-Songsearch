b846d00c3ecfe9cb24785cb43aefed6e8958ec73
PHD1222 <49057420+PHD1222@users.noreply.github.com>
2023-04-15 23:16:31 +0800

    Bug fixed

---
+++ js/values.js
@@ -0,0 +1,53 @@
+const diff_search_level = [
+    "0", "1", "2", "3", "4", "5", "6",
+    "7", "7+", "8", "8+", "9", "9+", "10", "10+",
+    "11", "11+", "12", "12+", "13", "13+", "14", "14+",
+    "15"
+];
+const diff_search = [
+    "Basic",
+    "Advanced",
+    "Expert",
+    "Master",
+    "Re:Master",
+    "All"
+];
+const diff_search_genre = [
+    "POPS&アニメ",
+    "niconico&ボーカロイド",
+    "東方Project",
+    "ゲーム&バラエティ",
+    "maimai",
+    "オンゲキ&CHUNITHM",
+    "All"
+];
+const diff_search_version = [
+    "maimai",
+    "maimai PLUS",
+    "maimai Green",
+    "maimai Green PLUS",
+    "maimai Orange",
+    "maimai Orange PLUS",
+    "maimai Pink",
+    "maimai Pink PLUS",
+    "maimai Murasaki",
+    "maimai Murasaki PLUS",
+    "maimai Milk",
+    "maimai Milk PLUS",
+    "maimai Finale",
+    "maimai DX",
+    "maimai DX PLUS",
+    "maimai DX Splash",
+    "maimai DX Splash PLUS",
+    "maimai DX UNiVERSE",
+    "maimai DX UNiVERSE PLUS",
+    "maimai DX FESTiVAL",
+    "All"
+];
+const diff_search_maptype = [
+    "ST", "DX", "All"
+];
+const diff_search_folder = [
+    "分类", "难度", "版本", "None"
+    //"名称"
+];
